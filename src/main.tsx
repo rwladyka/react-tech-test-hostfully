3b03b5556bebadd29f29a8eacd76dd2ebe065c40
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2023-11-27 16:05:27 -0300

    feat: add redux and ant providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ConfigProvider } from 'antd';
+import { Provider } from 'react-redux';
+import { store } from './store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <ConfigProvider theme={{ token: { colorPrimary: '#2d2aa5', borderRadius: 2 } }}>
+        <App />
+      </ConfigProvider>
+    </Provider>
+  </React.StrictMode>
+);
